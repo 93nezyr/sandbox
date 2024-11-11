c83f9ea4deddefe4d077d0890da7508d27829c20
93nezyr <julen5raqukm@gmail.com>
2024-11-11 16:02:48 +0900

    Add reward function

--- sandbox_24/24_2nd/bin/src/main.rs
+++ sandbox_24/24_2nd/bin/src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
     println!("Hello, world!");
+
+    let c = lib1::DefaultRewardNonLinear1::new(0, 0, 3.0, 3.0, 1);
+    println!("Goodnight, world!");
 }
--- sandbox_24/24_2nd/lib1/src/lib.rs
+++ sandbox_24/24_2nd/lib1/src/lib.rs
@@ -1,1 +1,4 @@
 mod py_lib_test;
+mod reward;
+
+pub use reward::DefaultRewardNonLinear1;
